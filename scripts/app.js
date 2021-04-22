e361df297dd03ddcda40a4af2baa2299a5917827
nightsky98525-dev <nightsky98525@gmail.com>
2021-04-22 14:34:39 +0800

    add tab

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,9 @@ var options = {
   captain: "Hello World",
   captainFontSize: 28,
   description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia dui lectus. Donec scelerisque ipsum diam, ac mattis orci pellentesque eget.",
+    ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
+    "Integer lacinia dui lectus.",
+    "Donec scelerisque ipsum diam, ac mattis orci pellentesque eget."],
   descritpionFontSize: 12
 };
 
@@ -21,56 +23,57 @@ var options = {
 
 var slideIndex = 1;
 document.addEventListener("DOMContentLoaded", function(e) {
-  showSlides(slideIndex);
+  console.log('start')
+  myWindowGlobalLibraryName.createSlideShow(options);
+  // showSlides(slideIndex);
 });
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var images = document.getElementsByClassName("imageClip");
-  var captains = document.getElementsByClassName("captain");
-  let descriptions = document.querySelectorAll(".description");
-  var dots = document.getElementsByClassName("dot");
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slide");
+//   var images = document.getElementsByClassName("imageClip");
+//   var captains = document.getElementsByClassName("captain");
+//   let descriptions = document.querySelectorAll(".description");
+//   var dots = document.getElementsByClassName("dot");
 
-  for (let i = 0; i < images.length; i++)
-    images[i].animate({ opacity: [0.4, 1] }, 1500);
-  for (let i = 0; i < captains.length; i++)
-    captains[i].animate({ opacity: [0, 1] }, { duration: 1000, delay: 1500 });
-  for (let i = 0; i < descriptions.length; i++)
-    descriptions[i].animate(
-      // [{ transform: "translate(0, 300%)" }, { transform: "none" }],
-      [{ transform: "translate(-100%, 0)" }, { transform: "none" }],
-      { duration: 1000, delay: 1500 }
-    );
+//   for (let i = 0; i < images.length; i++)
+//     images[i].animate({ opacity: [0.4, 1] }, 1500);
+//   for (let i = 0; i < captains.length; i++)
+//     captains[i].animate({ opacity: [0, 1] }, { duration: 1000, delay: 1000 });
+//   for (let i = 0; i < descriptions.length; i++)
+//     descriptions[i].animate(
+//       [{ transform: "translate(-100%, 0)" }, { transform: "none" }],
+//       { duration: 1000, delay: 1000 }
+//     );
 
-  if (n > images.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = images.length;
-  }
-  for (i = 0; i < images.length; i++) {
-    slides[i].style.display = "none";
-    images[i].style.display = "none";
-    captains[i].style.opacity = 0;
-    descriptions[i].style.opacity = 0;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  images[slideIndex - 1].style.display = "block";
-  setTimeout(() => {
-    captains[slideIndex - 1].style.opacity = 1;
-    descriptions[slideIndex - 1].style.opacity = 1;
-  }, 1600);
-  dots[slideIndex - 1].className += " active";
-}
+//   if (n > images.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = images.length;
+//   }
+//   for (i = 0; i < images.length; i++) {
+//     slides[i].style.display = "none";
+//     images[i].style.display = "none";
+//     captains[i].style.opacity = 0;
+//     descriptions[i].style.opacity = 0;
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   images[slideIndex - 1].style.display = "block";
+//   setTimeout(() => {
+//     captains[slideIndex - 1].style.opacity = 1;
+//     descriptions[slideIndex - 1].style.opacity = 1;
+//   }, 1100);
+//   dots[slideIndex - 1].className += " active";
+// }
--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,13 +1,125 @@
 (function(window) {
+  var slideIndex = 1;
+
   function multiSlideShow() {
     var _multiSlideShow = {};
 
     _multiSlideShow.createSlideShow = function(option) {
-      return console.log(option);
+      var slideShow = document.querySelector(".slideshow-container");
+      var panel = document.createElement("div");
+      var prev = document.createElement("a");
+      var next = document.createElement("a");
+      var dotContatiner = document.createElement("div");
+
+      panel.style.height = "100%";
+      panel.style.display = "flex";
+      panel.style.position = "absolute";
+
+      for (let i = 0; i < options.contents.length; i++) {
+        var slide = document.createElement("div");
+        var textArea = document.createElement("div");
+        var captain = document.createElement("div");
+        var description = document.createElement("div");
+        var img = document.createElement("img");
+        var dot = document.createElement("span");
+
+        slide.classList.add("slide");
+        img.setAttribute("src", options.contents[i]);
+        img.classList.add("imageClip");
+        textArea.classList.add("text-area");
+        captain.classList.add("captain");
+        captain.append(options.captain);
+        description.classList.add("description");
+
+        var sub = "";
+        for (let i = 0; i < options.description.length; i++) {
+          sub = sub + options.description[i] + "<br />";
+        }
+        description.innerHTML = sub;
+        dot.classList.add("dot");
+        dot.setAttribute("onclick", "currentSlide(" + (i + 1) + ")");
+
+        textArea.appendChild(captain);
+        textArea.appendChild(description);
+        slide.appendChild(img);
+        slide.appendChild(textArea);
+        dotContatiner.appendChild(dot);
+
+        panel.appendChild(slide);
+      }
+
+      prev.classList.add("prev");
+      // prev.addEventListener('click', plusSlides(-1))
+      prev.setAttribute("onclick", "plusSlides(-1)");
+      prev.innerHTML = "&#10094;";
+      next.classList.add("next");
+      next.setAttribute("onclick", "plusSlides(1)");
+      next.innerHTML = "&#10095;";
+      dotContatiner.classList.add("dot-container");
+
+      slideShow.appendChild(panel);
+      slideShow.appendChild(prev);
+      slideShow.appendChild(next);
+      slideShow.appendChild(dotContatiner);
+
+      fadeSlide(slideIndex);
     };
 
     return _multiSlideShow;
   }
+
+  // Click prev or push
+  this.plusSlides = function(n) {
+    fadeSlide((slideIndex += n));
+  };
+
+  // Click bottom button
+  this.currentSlide = function(n) {
+    fadeSlide((slideIndex = n));
+  };
+
+  // fade effect
+  this.fadeSlide = function(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var images = document.getElementsByClassName("imageClip");
+    var captains = document.getElementsByClassName("captain");
+    let descriptions = document.querySelectorAll(".description");
+    var dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < images.length; i++)
+      images[i].animate({ opacity: [0.4, 1] }, 1500);
+    for (let i = 0; i < captains.length; i++)
+      captains[i].animate({ opacity: [0, 1] }, { duration: 1000, delay: 1000 });
+    for (let i = 0; i < descriptions.length; i++)
+      descriptions[i].animate(
+        [{ transform: "translate(-100%, 0)" }, { transform: "none" }],
+        { duration: 1000, delay: 1000 }
+      );
+
+    if (n > images.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = images.length;
+    }
+    for (i = 0; i < images.length; i++) {
+      slides[i].style.display = "none";
+      images[i].style.display = "none";
+      captains[i].style.opacity = 0;
+      descriptions[i].style.opacity = 0;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    images[slideIndex - 1].style.display = "block";
+    setTimeout(() => {
+      captains[slideIndex - 1].style.opacity = 1;
+      descriptions[slideIndex - 1].style.opacity = 1;
+    }, 1200);
+    dots[slideIndex - 1].className += " active";
+  };
 
   if (typeof window.myWindowGlobalLibraryName === "undefined") {
     window.myWindowGlobalLibraryName = multiSlideShow();
